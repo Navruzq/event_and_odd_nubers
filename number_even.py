5c386e621570234ce4d23cfa320f839da67c155a
unknown <qarshiyevnavruz7777@gmil.com>
2022-10-20 10:47:57 +0500

    3 v

--- number_even.py
+++ number_even.py
@@ -3,3 +3,21 @@
 #Create a variable "var_int" and assign it a four-digit integer value.
 
 #Print the number of even digits in the variable "var_int".
+a=1357
+
+x1=a%10
+a=a//10
+
+x2=a%10
+a=a//10
+
+x3=a%10
+a=a//10
+
+x4=a%10
+a=a//10
+
+s=x1%2+x2%2+x3%2+x4%2-4
+s=s*(-1)
+print(s)
+
